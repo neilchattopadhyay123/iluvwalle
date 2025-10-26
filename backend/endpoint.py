6f67cb890c85320e86501eeb1a7d895c56683fc7
NicholasTa1 <nicholasneakta@gmail.com>
2025-10-26 00:05:21 -0400

    editing endpoint?

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -4,6 +4,9 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 import shutil
 import uvicorn
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
 app = FastAPI(
     title="Image Storage API",
@@ -63,7 +66,36 @@ async def upload_image(
             content={"error": f"Failed to save image: {str(e)}"}
         )
 
+# must install pip install -q -U google-genai
 
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+# Load .env file
+load_dotenv()
+
+# Access the API key
+api_key = os.environ.get("GEMINI_API_KEY")
+
+# Create client
+client = genai.Client(api_key=api_key)
+
+with open('backend/images/watterbottle1.png', 'rb') as f:
+      image_bytes = f.read()
+
+client = genai.Client()
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents=[
+      types.Part.from_bytes(
+        data=image_bytes,
+        mime_type='image/jpeg',
+      ),
+      'tell us if this is recyclable or not? respond with only recyclable or not recyclable.'
+    ]
+  )
+
+print(response.text)
 if __name__ == "__main__":
     # For local testing only. In production use a proper ASGI server.
     print("Starting FastAPI server...")
