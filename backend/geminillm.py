050d1e0636b5f2b8745a9f04f32e30b6d5b1a655
Brandon widjaja <bwidjaja05@gmail.com>
2025-10-25 21:07:15 -0400

    semi finished with gemini api by implementing files search

--- backend/geminillm.py
+++ backend/geminillm.py
@@ -5,16 +5,7 @@
 import os
 from google import genai
 from google.genai import types
-from PIL import Image
 from dotenv import load_dotenv
-
-
-# Load image
-#image_path = "C:/Users/phone/Downloads/metal_can.jpg"
-#image = Image.open(image_path)
-#buffered = io.BytesIO()
-#image.save(buffered, format="PNG")
-#img_b64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
 # Load .env file
 load_dotenv()
@@ -25,13 +16,21 @@ api_key = os.environ.get("GEMINI_API_KEY")
 # Create client
 client = genai.Client(api_key=api_key)
 
-# Upload image file
-my_file = client.files.upload(file="C:/Users/phone/Downloads/watterbottle1.png")
+with open('backend/images/watterbottle1.png', 'rb') as f:
+      image_bytes = f.read()
 
+client = genai.Client()
 response = client.models.generate_content(
-    model = "gemini-2.5-pro",
-    contents=[my_file, "is the item in the photo a recyclable item or not? The only response can be either 'recyclable' or 'not recyclable'." ],
-)
+    model='gemini-2.5-flash',
+    contents=[
+      types.Part.from_bytes(
+        data=image_bytes,
+        mime_type='image/jpeg',
+      ),
+      'tell us if this is recyclable or not? respond with only recyclable or not recyclable.'
+    ]
+  )
 
 print(response.text)
+
 
