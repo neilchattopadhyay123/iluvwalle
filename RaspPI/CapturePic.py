bbf43c42248fbf8b07bebf3410b798f230f29e4a
Imad Shaikh <imsanity9129@gmail.com>
2025-10-26 02:55:38 -0400

    v2 of rasp code

--- RaspPI/CapturePic.py
+++ RaspPI/CapturePic.py
@@ -1,15 +1,16 @@
 import cv2
 from datetime import datetime
-from pathlib import Path
 
 cap = cv2.VideoCapture(0)
-save_dir = Path("/home/pi/CamPics")
-save_dir.mkdir(parents=True, exist_ok=True)
+save_dir = "/home/pi/CamPics"
+
+# "/home/pi/CamPics"
+# "/Users/imadshaikh/Desktop/TempPics/"
 
 ret, frame = cap.read()
 if ret:
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    filename = save_dir / f"capture-{timestamp}.jpg"
+    filename = save_dir + f"capture-{timestamp}.jpg"
     cv2.imwrite(str(filename), frame)
 
 cap.release()
