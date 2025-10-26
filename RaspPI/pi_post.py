50f3b3ba1461e656e19506f0c2ba7ee2b35f8220
Brandon widjaja <bwidjaja05@gmail.com>
2025-10-26 03:51:15 -0400

    pi_post changes

--- RaspPI/pi_post.py
+++ RaspPI/pi_post.py
@@ -1,10 +1,9 @@
-import requests, base64
+import requests 
 
-url = 
+url = "http://mobile.psu.edu:8000/gemini-response"
 
 with open("phot.jpg", "rb") as f:
     img_data = {"file": f} # raw binary file
 
 response = requests.post(url, files=img_data)
 
-print(response.json()) #"url_to_your_endpoint"
