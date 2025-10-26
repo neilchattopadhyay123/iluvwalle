b9c00c98a5022e80614d034a5af9fac00455ba15
Imad Shaikh <imsanity9129@gmail.com>
2025-10-26 02:59:20 -0400

    rasp code v3

--- RaspPI/CapturePic.py
+++ RaspPI/CapturePic.py
@@ -4,7 +4,7 @@ from datetime import datetime
 cap = cv2.VideoCapture(0)
 save_dir = "/home/pi/CamPics"
 
-# "/home/pi/CamPics"
+# "/home/pi/CamPics" 1
 # "/Users/imadshaikh/Desktop/TempPics/"
 
 ret, frame = cap.read()
