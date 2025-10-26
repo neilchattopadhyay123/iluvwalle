3064917cc452a97f4a1c9718c504cd7276ea2196
Imad Shaikh <imsanity9129@gmail.com>
2025-10-26 03:08:47 -0400

    rasp code v4

--- RaspPI/CapturePic.py
+++ RaspPI/CapturePic.py
@@ -1,16 +1,16 @@
 import cv2
-from datetime import datetime
+import random
 
 cap = cv2.VideoCapture(0)
-save_dir = "/home/pi/CamPics"
+save_dir = "/home/pi/CamPics/"
 
-# "/home/pi/CamPics" 1
+# "/home/pi/CamPics/" 
 # "/Users/imadshaikh/Desktop/TempPics/"
 
 ret, frame = cap.read()
 if ret:
-    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    filename = save_dir + f"capture-{timestamp}.jpg"
+    random_integer = random.randint(0, 1000000)
+    filename = save_dir + f"capture-{random_integer}.jpg"
     cv2.imwrite(str(filename), frame)
 
 cap.release()
